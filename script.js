9050127e43f677363a19857b4b2f925e35d800af
SergioVed <vedankinsergij@gmail.com>
2023-09-05 22:04:11 +0300

    Third commit

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let intervalTime = 0;
 let interval = 0;
 let score = 0;
 let speed = 0.8;
+let body = [[1, 6], [1, 7], [1, 8], [1, 9], [2, 9]];
 
 function createBoard(){
     for(let i = 0; i < 100; i++){
@@ -14,15 +15,14 @@ function createBoard(){
 }
 createBoard()
  class Game{
-    constructor(){
-        this.score = score;
-        this.speed = speed;
-        this.firection = direction;
+    constructor(game){
+        this.score = game.score;
+        this.speed = game.speed;
+        this.direction = game.direction;
     }
  }
-console.log(score, speed)
 class Snake{
-    constructor(){
-        this.body = [[1, 6], [1, 7], [1, 8], [1, 9], [2, 9]];
+    constructor(snake){
+        this.body = snake.body;  
     }
 }
