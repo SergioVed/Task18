c8f59327eda49b3d661dd4da33a32a9b886cb491
SergioVed <vedankinsergij@gmail.com>
2023-09-05 21:12:24 +0300

    Second commit

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let grid = document.querySelector(".grid");
 let scoreDisplay = document.querySelector(".scoreDisplay")
 let width = 10;
-let curentSnake = [2, 1, 0];
 let intervalTime = 0;
 let interval = 0;
+let score = 0;
+let speed = 0.8;
 
 function createBoard(){
     for(let i = 0; i < 100; i++){
@@ -12,3 +13,16 @@ function createBoard(){
     }
 }
 createBoard()
+ class Game{
+    constructor(){
+        this.score = score;
+        this.speed = speed;
+        this.firection = direction;
+    }
+ }
+console.log(score, speed)
+class Snake{
+    constructor(){
+        this.body = [[1, 6], [1, 7], [1, 8], [1, 9], [2, 9]];
+    }
+}
